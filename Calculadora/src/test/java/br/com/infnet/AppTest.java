061c9b723954dd769562274caa9a08461e5dc509
Raquel Braga <raquel.santos@al.infnet.edu.br>
2023-12-11 22:26:20 -0300

    versao TP3

--- Calculadora/src/test/java/br/com/infnet/AppTest.java
+++ Calculadora/src/test/java/br/com/infnet/AppTest.java
@@ -1,8 +1,11 @@
 package br.com.infnet;
 
+import funcoes.Funcoes;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+
 
 /**
  * Unit test for simple App.
@@ -10,27 +13,32 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+/** @Test
+   public void testSomarValores(){
+      double y = 1;
+      double x = 2;
+      Funcoes instance = new Funcoes();
+      double expResult = 3;
+      double result = instance.somarValores(y,x);
+      assertEquals(expResult, result, 0);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
+}**/
+@Test
+System.out.println("Deve retornar o codigo 200");
+public void testaCodigo200(){
+    Funcoes i = new Funcoes();
+    int retorno = Funcoes.somarValores(double x,double y,double z);
+    assertEquals(200,retorno);
+
+}
+@Test
+        System.out.println("Deve retornar o codigo 200");
+    public void testaCodigo200() {
+        Funcoes j = new Funcoes();
+        int retorno = Funcoes.dividirValores( double x, double y, double z);
+        assertEquals(200, retorno);
+    }
     public void testApp()
     {
         assertTrue( true );
